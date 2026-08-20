d7f4a9d7b0bd8a7eae310c72f6f198a7436e86c9
rsenadheerascout-afk <rsenadheera.scout@gmail.com>
2026-08-20 11:52:40 +0530

    Added sizes props to image with src in FeaturedCoursesSection

--- components/sections/home/FeaturedCoursesSection.tsx
+++ components/sections/home/FeaturedCoursesSection.tsx
@@ -34,6 +34,7 @@ function FeaturedCoursesSection({ featuredCourses }: featureCourseSectionProps) 
                   src={course.image}
                   alt={course.title}
                   fill
+                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                   className="object-cover transition-transform duration-500 group-hover:scale-105 rounded-2xl"
                 />
 
