8bc36199ac11f47a6afe24b37fcee4d25bec5e30
rsenadheerascout-afk <rsenadheera.scout@gmail.com>
2026-08-25 15:40:29 +0530

    Duplicate record in testimonials to activate the carousal effect

--- data/home.ts
+++ data/home.ts
@@ -69,4 +69,12 @@ export const testimonials = [
       company: "iSeeQ",
     },
   },
+  {
+    text: "The quality of the courses and support from the academy made the entire learning journey worthwhile.",
+    author: {
+      name: "Ben-2",
+      avatarUrl: "",
+      company: "iSeeQ",
+    },
+  },
 ];
