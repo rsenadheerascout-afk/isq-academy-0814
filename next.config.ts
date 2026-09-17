45363824699bf646b91f760776a1f476901c65fb
amrusajahan <amrusajahan2@gmail.com>
2026-09-17 12:26:05 +0530

    config: add config options to host the website for local network in dev env. (Commented)

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,26 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
+  // Allow dev server connections from your local network IP
+  // experimental: {
+  //   serverActions: {
+  //     allowedOrigins: ['192.168.8.167:3000', 'localhost:3000'],
+  //   },
+  // },
+  // webpack: (config, { dev }) => {
+  //   if (dev) {
+  //     config.watchOptions = {
+  //       poll: 1000,
+  //       aggregateTimeout: 300,
+  //     };
+  //   }
+  //   return config;
+  // },
+
+  /* 
+  run the below after uncommenting above
+  > npx next dev --webpack -H 192.168.8.167 
+   */
 };
 
 export default nextConfig;
