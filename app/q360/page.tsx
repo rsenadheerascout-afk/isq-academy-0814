a09ae08dcc1e482a3a1934130b831e2acdc2bd8f
amrusajahan <amrusajahan2@gmail.com>
2026-09-17 12:29:07 +0530

    fix: change body element into div element, body cannot be a child of a body

--- app/q360/page.tsx
+++ app/q360/page.tsx
@@ -11,11 +11,11 @@ export const metadata: Metadata = {
 
 export default function Q360Page() {
   return (
-      <body className="flex flex-col h-screen">
-  <div className="flex flex-col items-center justify-center grow bg-linear-to-tr from-primary to-secondary gap-2">
-    <h1 className="text-6xl text-black">Q360°</h1>
-    <p className="text-2xl text-black">Coming Soon...</p>
-  </div>
-</body>
+    <div className="flex flex-col h-screen">
+      <div className="flex flex-col items-center justify-center grow bg-linear-to-tr from-primary to-secondary gap-2">
+        <h1 className="text-6xl text-black">Q360°</h1>
+        <p className="text-2xl text-black">Coming Soon...</p>
+      </div>
+    </div>
   );
 }
